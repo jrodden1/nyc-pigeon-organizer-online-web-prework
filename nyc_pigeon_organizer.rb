4244646a04fde067d477762066394c88d1f6159f
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-06 20:56:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,9 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |attribute, nestHash|
-    nestHash.each do |key, valueArr|
-      valueArr.each do |value|
+  data.map do |attribute, nestHash|
+    nestHash.map do |key, valueArr|
+      valueArr.map do |value|
         if pigeon_list.has_key?(value) == false
           pigeon_list[value] = {attribute => [key.to_s]}
         elsif pigeon_list[value].has_key?(key) == false
