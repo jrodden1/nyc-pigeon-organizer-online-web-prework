8c783b3ecb9c7e75501be30e0dcacb0b929ecc72
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-06 20:18:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |masterHash, nestHash|
     nestHash.each do |key, valueArr|
-      binding.pry
+      valueArr.each do |value|
+        binding.pry
+      end
     end
   end
 end
