b53b69c98cd0e955c8612a311bfc0eda5c3ce778
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-06 20:36:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,9 @@ def nyc_pigeon_organizer(data)
         if pigeon_list.has_key?(value) == false
           pigeon_list[value] = {attribute => key.to_s}
         elsif pigeon_list[value].has_key?(key) == false
-          pigeon_list[value][attribute]
+          #pigeon_list[value][attribute][key] =
+          binding.pry
         end
-        binding.pry
       end
     end
   end
