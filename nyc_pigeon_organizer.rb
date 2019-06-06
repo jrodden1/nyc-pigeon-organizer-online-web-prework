bac8b2439662febccdd81c71c34bf0068e104a35
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-06 20:37:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     nestHash.each do |key, valueArr|
       valueArr.each do |value|
         if pigeon_list.has_key?(value) == false
-          pigeon_list[value] = {attribute => key.to_s}
+          pigeon_list[value] = {attribute => [key.to_s]}
         elsif pigeon_list[value].has_key?(key) == false
           pigeon_list[value][attribute] << key.to_s
           binding.pry
