1e107daa845ec5d461ef1a3eb9a7ba7ac061adf4
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-06 21:07:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
       valueArr.map do |value|
         if pigeon_list.has_key?(value) == false
           pigeon_list[value] = {attribute => [key.to_s]}
-        elsif pigeon_list[value].has_key?(key) == false
+        elsif pigeon_list[value].has_key?(attribute) == false
           puts key.to_s
           pigeon_list[value] = {attribute => [key.to_s]}
           binding.pry
