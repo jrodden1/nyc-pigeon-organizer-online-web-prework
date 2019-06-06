ba2655e306dcdc7bae0cc7604b649b5db1e33f6f
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-06 20:49:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,16 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attribute, nestHash|
-    attribute.each do |attribName|
-      nestHash.each do |key, valueArr|
-        valueArr.each do |value|
-          if pigeon_list.has_key?(value) == false
-            pigeon_list[value] = {attribute => [key.to_s]}
-          elsif pigeon_list[value].has_key?(key) == false
-            pigeon_list[value][attribute] << key.to_s
-          end
+    nestHash.each do |key, valueArr|
+      valueArr.each do |value|
+        if pigeon_list.has_key?(value) == false
+          pigeon_list[value] = {attribute => [key.to_s]}
+        elsif pigeon_list[value].has_key?(key) == false
+          pigeon_list[value][attribute] << key.to_s
         end
       end
-    binding.pry
     end
+  binding.pry
   end
 
   pigeon_list
